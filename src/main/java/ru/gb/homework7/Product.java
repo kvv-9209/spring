b3c28a82d79c62a481c9dd787c07595edd0fac8e
kvv-9209 <kvv-9209@mail.ru>
2022-04-29 12:47:27 +0300

    Homework7

---
+++ src/main/java/ru/gb/homework7/Product.java
@@ -0,0 +1,24 @@
+package ru.gb.homework7;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "cost")
+    private BigDecimal cost;
+}
